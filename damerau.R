37856edb0f764c8b1f4ff07beee1f56ef4b5f1b6
sulki <sulkipark828@tamu.edu>
2017-01-28 15:43:03 -0600

    damerau-levenshtein distance R code

--- damerau.R
+++ damerau.R
@@ -1,7 +1,8 @@
-#adist("kitten", "sitting") #the function for levenshtein distance
-#str1 = c("kitten"); str2 = c("sitting") #example strings
+#library(stringdist)
+#stringdist("animal","ainmal",method="dl") #the function for damerau-levenshtein distance
+#str1 = c("animal"); str2 = c("ainmal") #example strings
 
-lv.dist = function(str1, str2) {
+d.dist = function(str1, str2) {
   spstr1 = strsplit(str1,"")[[1]]; spstr2 = strsplit(str2,"")[[1]] #form the string
   len1 = length(spstr1); len2 = length(spstr2) #length of strings
   D <- matrix(nrow=len2+1, ncol=len1+1) #make a distance matrix
@@ -13,10 +14,16 @@ lv.dist = function(str1, str2) {
       frpj = D[i,j-1] + 1
       frpij = D[i-1,j-1] + ifelse(spstr1[j-1]!=spstr2[i-1],1,0)
       D[i,j] = min(frpi,frpj,frpij) #get the minimum value of three:frpi, frpj, frpij
+      
+      if (i>2 && j>2 && spstr1[j-1]==spstr2[i-2] && spstr1[j-2]==spstr2[i-1]) {
+        fr2pij = D[i-2,j-2] + 1
+        D[i,j] = min(D[i,j], fr2pij) #transposition
+      }
+    
     }
   }
-  return(D[len2+1,len1+1]) #get levenshtein distance: i from str2, j from str1
+  return(D[len2+1,len1+1]) #get damerau-levenshtein distance: i from str2, j from str1
 }
-  
-lv.dist("kitten", "sitting") #the result is same as adist function
-  
+
+d.dist("animal", "ainmal") #correct answer=1
+lv.dist("animal", "ainmal") #to confirm: answer=2
